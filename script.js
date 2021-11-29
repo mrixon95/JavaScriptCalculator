b84760a08b77a1509a22b2338b99fde968ea1f37
[Michael Rixon s3827641] <s3827641@student.rmit.edu.au>
2021-11-30 00:28:33 +1100

    Almost completed equals event listener

--- script.js
+++ script.js
@@ -1,17 +1,70 @@
 const numberButtons = document.querySelectorAll('[data-number]');
 const operationButtons = document.querySelectorAll('[data-operation]');
-const equalsButtons = document.querySelector('[data-equals]');
-const deleteButtons = document.querySelector('[data-delete]');
+const equalsButton = document.querySelector('[data-equals]');
+const deleteButton = document.querySelector('[data-delete]');
 const allClearButton = document.querySelector('[data-all-clear]');
 const previousOperandTextElement = document.querySelector('[data-previous-operand]');
 const currOperandTextElement = document.querySelector('[data-current-operand]');
 
+var currentValue = 0
+var currentOperator = null;
+
+equalsButton.addEventListener('click', event => {
+    currOperandTextElement.innerHTML = currentValue;
+    currentValue = 0;
+
+});
 
 numberButtons.forEach(item => {
     item.addEventListener('click', event => {
+        var numAsStr = item.innerHTML;
+        var num = parseInt(numAsStr);
+
         currOperandTextElement.innerHTML += item.innerHTML;
+        if (currentOperator != null) {
+            switch(currentOperator) {
+                case '+':
+                    currentValue += num;
+                    break;
+                case '-':
+                    currentValue -= num;
+                    break;
+                case '*':
+                    currentValue *= num;
+                    break;
+                case '/':
+                    currentValue /= num;
+                    break;
+            }
+            currentOperator = null;
+        }
     }
     )
+});
+
+operationButtons.forEach(item => {
+    item.addEventListener('click', event => {
+        
+        if (item.innerHTML == ".") {
+            currOperandTextElement.innerHTML += ".";
+        } else {
+            currOperandTextElement.innerHTML += " " + item.innerHTML + " ";
+            currentOperator = currOperandTextElement.innerHTML;
+        }
+        
+    }
+    )
+});
+
+
+deleteButtons.addEventListener('click', event => {
+    let currText = currOperandTextElement.innerHTML;
+    currOperandTextElement.innerHTML = currText.substring(0,currText.length - 1);
+});
+
+deleteButtons.addEventListener('click', event => {
+    let currText = currOperandTextElement.innerHTML;
+    currOperandTextElement.innerHTML = currText.substring(0,currText.length - 1);
 });
 
 
